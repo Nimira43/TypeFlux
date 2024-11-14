2ccb480697f92cbf8b7de378b5f518cdf2204960
Nimira43 <n.rabone72@aol.com>
2024-11-14 22:16:08 +0000

    Adding a way to update the user.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { User } from './models/User'
 
 const user = new User({ name: 'Bob', age: 58 })
 
+user.set({ name: 'Bill', age: 64 })
+
 console.log(user.get('name'))
 console.log(user.get('age'))

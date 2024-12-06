539b9a3e78859d47b231522bd8fc9f549ca93814
Nimira43 <n.rabone72@aol.com>
2024-12-06 18:58:26 +0000

    Creating more events

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { User } from './models/User'
 
 const user = new User({ name: 'Bob', age: 58 })
 
-user.on('change', () => {
-
-})
+user.on('change', () => {})
+user.on('create', () => {})
+user.on('delete', () => {})
 
 console.log(user)

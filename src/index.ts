53dc91fb20ca23b60898e3d3b4842eb5e9cbfc00
Nimira43 <n.rabone72@aol.com>
2025-02-09 15:17:22 +0000

    Cleared out index.ts - imported axios. - will create REST functionality for CRUD operations.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import { User } from './models/User'
+import axios from 'axios'
 
-const user = new User({ name: 'Bob', age: 58 })
-
-user.on('change', () => {
-  console.log('Change 1')
-})
-user.on('change', () => {
-  console.log('Change 2')
-})
-user.on('save', () => {
-  console.log('User Saved!')
-})
-
-user.trigger('save')
+axios.post('')

f6a2bd83e42d37b07fe49116230bd313aaeddfc7
Nimira43 <n.rabone72@aol.com>
2025-02-23 23:38:36 +0000

    Using AxiosPromise to  ensure that both fetch and save methods return promises, making it easier to handle asynchronous operations.

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,24 +1,20 @@
-import axios, { AxiosResponse } from 'axios'
+import axios, { AxiosPromise } from 'axios'
 import {UserProps } from './User'
 
 export class Sync {
   constructor(public rootUrl: string) {}
 
-  fetch(id: number): void {
-    axios.get(`${this.rootUrl}/${id}`).then(
-      (response: AxiosResponse): void => {
-      this.set(response.data)
-      }
-    )
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`)
   }
   
-  save(data: UserProps): void {
+  save(data: UserProps): AxiosPromise {
     const { id } = data
 
     if (id) {
-      axios.put(`${this.rootUrl}/${id}`, data)
+      return axios.put(`${this.rootUrl}/${id}`, data)
     } else {
-      axios.post(this.rootUrl, data)
+      return axios.post(this.rootUrl, data)
     }
   }
 }

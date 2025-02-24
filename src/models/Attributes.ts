198f9ccea0e1bbfcbd89c61ad503851bbcb77eb9
Nimira43 <n.rabone72@aol.com>
2025-02-24 03:25:46 +0000

    Brought get and set methods into Attributes from User.ts

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,3 +1,11 @@
 export class Attributes {
+  constructor(private data: UserProps) { }
 
+  get(propName: string): (number | string) {
+    return this.data[propName]
+  }
+  
+  set(update: UserProps): void {
+    Object.assign(this.data, update)
+  }
 }

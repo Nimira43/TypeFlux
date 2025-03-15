51c90e438f070afe852493b1bd4c422dee8a1896
Nimira43 <n.rabone72@aol.com>
2025-03-15 15:19:57 +0000

    Created the Person class with firstName and lastName properties that are both public and initialised via the constructor

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { User } from './models/User'
 
 const user = new User({ name: 'Sylvester', age: 63 })
 
-user.events.on('change', () => {
-  console.log('Changed - The Nesting Way!!!!')
-})
-
-user.events.trigger('change')
+class Person {
+  constructor(
+    public firstName: string,
+    public lastName: string
+  ) {}
+} 

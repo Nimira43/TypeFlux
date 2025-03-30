654aa6bed0802b375204f8ddce91f4e6436bba84
Nimira43 <n.rabone72@aol.com>
2025-03-30 18:46:23 +0100

    User class is now based on Model with UserProps as it's type.

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,7 +12,13 @@ export interface UserProps {
 const rootUrl = 'http://localhost:3000/users'
 
 export class User extends Model<UserProps> {
-
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    )
+  }
 }
 
 

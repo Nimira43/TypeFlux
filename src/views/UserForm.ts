9271d124b48469c4a7411ee1a57b487991b9d5a8
Nimira43 <n.rabone72@aol.com>
2025-04-19 23:48:33 +0100

    The render method effectively injects the template into the parent while ensuring events are bound to the elements inside.

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -37,6 +37,7 @@ export class UserForm {
   render(): void {
     const templateElement = document.createElement('template')
     templateElement.innerHTML = this.template()
+    this.bindEvents(templateElement.content)
     this.parent.append(templateElement.content)
   }
 }

7c7bfee2788cb1bf47b1d7f353a6e109791bdbc9
Nimira43 <n.rabone72@aol.com>
2025-04-19 21:31:56 +0100

    Utilising Collection class to fetch user data from the JSON server at http://localhost:3000/users

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@
 
 // npm run start:db
 
-import axios, { AxiosResponse } from 'axios'
+import { Collection } from './models/Collection';
 
-axios.get('http://localhost:3000/users')
-  .then((response: AxiosResponse) => {
-    console.log(response.data)
-  })
+const collection = new Collection('http://localhost:3000/users')
+
+collection.fetch()

f9c66e01bad63863142bd63a6d316cb29cc444ff
Nimira43 <n.rabone72@aol.com>
2025-04-19 21:42:51 +0100

    The collection.on('change') method ensures that whenever the fetch method triggers a change event, the collection is logged to the console.

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,9 @@ import { Collection } from './models/Collection';
 
 const collection = new Collection('http://localhost:3000/users')
 
+collection.on('change', () => {
+  console.log(collection)
+})
+
+
 collection.fetch()

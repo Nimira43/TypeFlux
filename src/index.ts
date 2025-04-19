642d1dac4f65b4733ad9ccb142596a0b935ddb39
Nimira43 <n.rabone72@aol.com>
2025-04-19 21:10:24 +0100

    Inclusion of on and trigger as getters streamlines event handling for the collection. This means app can easily subscribe to changes and respond dynamically. Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@
 
 // npm run start:db
 
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
+axios.get('http://localhost:3000/users')
+  .then((response: AxiosResponse) => {
+    console.log(response.data)
+  })

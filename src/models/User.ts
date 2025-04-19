d1980f34bbc696ab595c682bba6e116bc6cd6c67
Nimira43 <n.rabone72@aol.com>
2025-04-19 22:11:59 +0100

    Adding the static buildUserCollection() method to the User class, streamlining the process of creating Collection instances tailored to the User model.

--- src/models/User.ts
+++ src/models/User.ts
@@ -20,6 +20,12 @@ export class User extends Model<UserProps> {
       new ApiSync<UserProps>(rootUrl)
     )
   }
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(
+      rootUrl,
+      (json: UserProps) => User.buildUser(json)
+    )
+  }
 }
 
 

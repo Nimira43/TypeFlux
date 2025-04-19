aac556d48a611e0981a86a3fb41230788a4da423
Nimira43 <n.rabone72@aol.com>
2025-04-19 22:23:32 +0100

    Starting  on the User Form

---
+++ src/views/UserForm.ts
@@ -0,0 +1,12 @@
+export class UserForm {
+  parent: Element
+  template(): string {
+    return `
+      <div>
+        <h1>TypeFlux User Form</h1>
+        <input />
+      </div>  
+    
+    `
+  }
+}

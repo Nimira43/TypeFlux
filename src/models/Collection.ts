ddc10dca11cc5307150661308c9a029e641d134f
Nimira43 <n.rabone72@aol.com>
2025-04-19 21:21:52 +0100

    Fetch method allows you to dynamically retrieve data from the API based on the rootUrl.

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,10 +1,13 @@
 import axios from 'axios'
 import { User, UserProps } from './User'
 import { Eventing } from './Eventing'
+import { AxiosResponse } from 'axios'
 
 export class Collection {
   models: User[] = []
   events: Eventing = new Eventing()
+
+  constructor(public rootUrl: string) {}
 
   get on() {
     return this.events.on
@@ -12,5 +15,12 @@ export class Collection {
   
   get trigger() {
     return this.events.trigger
+  }
+
+  fetch(): void {
+    axios.get(this.rootUrl)
+      .then((response: AxiosResponse) => {
+      response.data
+    })
   }
 }

ec21f9bfb6f5b932f905e13cf6debe9d02f2cebf
Nimira43 <n.rabone72@aol.com>
2025-06-10 11:24:37 +0100

    Moved bindEvents and render into View.ts

--- src/views/View.ts
+++ src/views/View.ts
@@ -11,4 +11,24 @@ export abstract class View {
       this.render()
     })
   }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap()
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':')
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey])
+      })
+    }
+  }
+
+  render(): void {
+    this.parent.innerHTML = ''
+    const templateElement = document.createElement('template')
+    templateElement.innerHTML = this.template()
+    this.bindEvents(templateElement.content)
+    this.parent.append(templateElement.content)
+  }
 }

44ce27a0ade81ff2d54d8cf2b6bed2f4c5ef68a8
Nimira43 <n.rabone72@aol.com>
2025-06-10 11:19:06 +0100

    Archiving current progress.

---
+++ archive/TypeFlux-1/src/index.ts
@@ -0,0 +1,24 @@
+// Don't forget to start the JSON Server when coming back to the project!!!
+
+// npm run start:db
+
+import { UserForm } from './views/UserForm'
+import { User } from './models/User'
+
+const user = User.buildUser({
+  name: 'Liam',
+  age: 53
+})
+
+const root = document.getElementById('root')
+
+if (root) {
+  const userForm = new UserForm(root, user)
+  userForm.render()
+} else {
+  throw new Error('Root element not found.')
+}
+
+
+
+

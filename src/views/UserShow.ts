a2cf9294e65991b5559175b607455c58090d1035
Nimira43 <n.rabone72@aol.com>
2025-07-03 13:52:32 +0100

    Create template for UserShow

--- src/views/UserShow.ts
+++ src/views/UserShow.ts
@@ -2,5 +2,13 @@ import { View } from './View'
 import { User, UserProps } from '../models/User' 
 
 export class UserShow extends View<User, UserProps> {
-  
+  template(): string {
+    return `
+      <div>
+        <h1>User Detail</h1>
+        <div>Username: ${this.model.get('name')}</div>
+        <div>Age: ${this.model.get('age')}</div>
+      </div>
+    `
+  }
 }

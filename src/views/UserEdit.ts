a32314d947205f8501715d72e8faf9977159c989
Nimira43 <n.rabone72@aol.com>
2025-07-03 14:06:12 +0100

    Defining regions prop

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -2,12 +2,12 @@ import { View } from './View'
 import { User, UserProps } from '../models/User'
 
 export class UserEdit extends View<User, UserProps> {
-    template() {
-      return `
-        <div>
-          <div class='user-show'></div>
-          <div class='user-form'></div>
-        </div>
-      `
-    }
+  template() {
+    return `
+      <div>
+        <div class='user-show'></div>
+        <div class='user-form'></div>
+      </div>
+    `
+  }
 }
--- src/views/View.ts
+++ src/views/View.ts
@@ -1,6 +1,8 @@
 import { Model, HasId } from '../models/Model'
 
 export abstract class View<T extends Model<K>, K extends HasId> {
+  regions: { [key: string]: Element } = {}
+
   constructor(
     public parent: Element,
     public model: T

dafa3796801eac46aeda988e739a5490bab56aa7
Nimira43 <n.rabone72@aol.com>
2025-07-29 08:55:26 +0100

    Using renderItem

--- src/views/UserList.ts
+++ src/views/UserList.ts
@@ -1,6 +1,8 @@
 import { CollectionView } from './CollectionView'
 import { User, UserProps } from '../models/User'
 
-export class UserList extends CollectionView {
-
+export class UserList extends CollectionView<User, UserProps> {
+  renderItem(model: User, itemParent: Element) {
+    
+  }
 }

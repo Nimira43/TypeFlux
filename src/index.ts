7e0d5675dd30977e6b9af4eebf93cec7e1866519
Nimira43 <n.rabone72@aol.com>
2025-07-29 09:00:13 +0100

    Clearing out index.ts. Bringing in imports

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,9 @@
 
 // npm run start:db
 
-import { UserEdit } from './views/UserEdit'
-import { User } from './models/User'
-
-const user = User.buildUser({
-  name: 'Name',
-  age: 18
-})
-const root = document.getElementById('root')
-
-if (root) {
-  const userEdit = new UserEdit(root, user)
-  userEdit.render()
-  console.log(userEdit)
-} else {
-  throw new Error('Root element not found.')
-}
+import { UserList } from './views/UserList'
+import { Collection } from './models/Collection'
+import { UserProps } from './models/User'
 
 
 

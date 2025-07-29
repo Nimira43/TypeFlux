268f620735a8d8a88c29c1b636ca0c2083f6dd12
Nimira43 <n.rabone72@aol.com>
2025-07-29 09:08:14 +0100

    REndering all the users to the screen.

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,16 @@ const users = new Collection('http://localhost:3000/users', (json: UserProps) =>
   return User.buildUser(json)
 })
 
+users.on('change', () => {
+  const root = document.getElementById('root')
+
+  if (root) {
+    new UserList(root, users).render()
+  }
+})
+
+users.fetch()
+
 
 
 

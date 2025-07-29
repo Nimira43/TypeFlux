7faf863da7a0782c3c32e0cd4fa13460cec87808
Nimira43 <n.rabone72@aol.com>
2025-07-29 09:04:23 +0100

    Creating users

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@
 
 import { UserList } from './views/UserList'
 import { Collection } from './models/Collection'
-import { UserProps } from './models/User'
+import { User, UserProps } from './models/User'
+
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+  return User.buildUser(json)
+})
 
 
 
